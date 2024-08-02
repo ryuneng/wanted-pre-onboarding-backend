fb2ac5abaffb58cab8789fc3573683889d9361c4
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-08-02 20:57:15 +0900

    Feat: Swagger 연동 설정

---
+++ src/main/java/com/wanted/preonboardingbackend/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.wanted.preonboardingbackend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("wanted-pre-onboarding-backend API Documentation")
+                .description("원티드 프리온보딩 백엔드 인턴십 선발과제 API 문서")
+                .version("1.0");
+    }
+}
