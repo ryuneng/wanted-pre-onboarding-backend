5d7c542d204820061c84369793c255a1542a2e0e
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-08-07 14:09:54 +0900

    Test: Presentation Layer 테스트 - 채용공고 수정, 삭제(해피케이스)

--- src/test/java/com/wanted/preonboardingbackend/domain/jobPosting/controller/JobPostingControllerTest.java
+++ src/test/java/com/wanted/preonboardingbackend/domain/jobPosting/controller/JobPostingControllerTest.java
@@ -2,6 +2,7 @@ package com.wanted.preonboardingbackend.domain.jobPosting.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wanted.preonboardingbackend.domain.jobPosting.dto.JobPostingSaveRequestDto;
+import com.wanted.preonboardingbackend.domain.jobPosting.dto.JobPostingUpdateRequestDto;
 import com.wanted.preonboardingbackend.domain.jobPosting.service.JobPostingService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(JobPostingController.class)
@@ -27,9 +30,9 @@ public class JobPostingControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Test
     @DisplayName("채용공고를 등록한다.")
-    void saveJob() throws Exception {
+    @Test
+    void saveJobPosting() throws Exception {
         // given
         JobPostingSaveRequestDto request = JobPostingSaveRequestDto.builder()
                 .companyId(1L)
@@ -47,5 +50,49 @@ public class JobPostingControllerTest {
                 )
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isCreated());
+
+    }
+
+    @DisplayName("채용공고를 수정한다.")
+    @Test
+    void updateJobPosting() throws Exception {
+        // given
+        JobPostingUpdateRequestDto request = JobPostingUpdateRequestDto.builder()
+                .position("풀스택 개발자")
+                .reward(3000)
+                .content("풀스택 개발자 채용합니다.")
+                .skill("Java, Vue.js")
+                .build();
+
+        // when // then
+        mockMvc.perform(
+                put("/job/" + 1L)
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("채용공고를 삭제한다.")
+    @Test
+    void deleteJobPosting() throws Exception {
+        // given
+        Long jobPostingId = 1L;
+
+        // 서비스 메서드를 모킹하여 실제 메서드 호출 대신 원하는 동작 시뮬레이션
+        doNothing().when(jobPostingService).delete(jobPostingId);
+
+        // when // then
+        mockMvc.perform(
+                delete("/job/" + jobPostingId)
+                        .content(objectMapper.writeValueAsString(jobPostingId))
+                        .contentType(MediaType.APPLICATION_JSON)
+                    )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());
+
+        // 서비스 메서드 호출 여부 검증
+        verify(jobPostingService).delete(jobPostingId);
     }
 }
